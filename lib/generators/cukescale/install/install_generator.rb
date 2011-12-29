94b4b76a842417db38b9957c6ee7f6f25f9f2707
Calvin Delamere <calvin@coshx.com>
2011-12-29 15:00:17 -0700

    generators create a config file and rake tasks

--- lib/generators/cukescale/install/install_generator.rb
+++ lib/generators/cukescale/install/install_generator.rb
@@ -1,8 +1,17 @@
 module Cukescale
   class InstallGenerator < Rails::Generators::Base
+    #Cukescale::InstallGenerator.source_root(File.expand_path("../templates", __FILE__))
+    source_root File.expand_path("../templates", __FILE__)
+
     #TODO generate stuff
-    def create_initializer_file
-      empty_directory 'features'
+    def create_tasks
+      empty_directory 'lib/tasks'
+      template 'tasks/cukescale.rake.erb', 'lib/tasks/cukescale.rake'
     end
+
+    def create_templates
+      template 'config/cukescale.yml.erb', 'config/cukescale.yml'
+    end
+
   end
 end
