56e6fd953b8cf58a8644b5bc4cd93d43a4ebc35e
Gabe Kopley <gabe@coshx.com>
2011-12-29 23:11:34 -0700

    configure in generated ruby file ala cucumber

--- lib/generators/cukescale/install/install_generator.rb
+++ lib/generators/cukescale/install/install_generator.rb
@@ -3,14 +3,17 @@ module Cukescale
     #Cukescale::InstallGenerator.source_root(File.expand_path("../templates", __FILE__))
     source_root File.expand_path("../templates", __FILE__)
 
+    def create_env_rb
+      empty_directory 'test'
+      empty_directory 'test/scalability'
+      empty_directory 'test/scalability/support'
+      template 'support/env.rb', 'test/scalability/support/env.rb'
+    end
+
     #TODO generate stuff
     def create_tasks
       empty_directory 'lib/tasks'
       template 'tasks/cukescale.rake.erb', 'lib/tasks/cukescale.rake'
-    end
-
-    def create_templates
-      template 'config/cukescale.yml.erb', 'config/cukescale.yml'
     end
 
   end
